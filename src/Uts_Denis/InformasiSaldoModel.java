a3b5c2b2cdaef9ec5e2c25f16c222e93f35406af
Denis Yusri Hyuanda <denisyusrihyuanda03@gmail.com>
2023-06-08 12:52:47 +0700

    UTS Denis Yusri 2101092013

---
+++ src/Uts_Denis/InformasiSaldoModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Uts_Denis;
+import Uts_Denis.TransferModel;
+
+/**
+ *
+ * @author ASUS
+ */
+public class InformasiSaldoModel {
+    String saldo="1000000";
+    
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    
+}
